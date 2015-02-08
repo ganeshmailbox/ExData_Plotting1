37b70930084d4ad10e15246935f417a09a441954
Ganesh Sethuraman <ganeshmailbox@gmail.com>
2015-02-08 18:33:17 -0500

    plot 4

--- plot4.R
+++ plot4.R
@@ -4,10 +4,15 @@ str(dataSet)
 dataSet <- data[data$Date == "1/2/2007" | data$Date == "2/2/2007",] 
 dataSet$datetime<-as.POSIXct(paste(dataSet$Date,dataSet$Time), format="%d/%m/%Y %H:%M:%S")
 
-## Create and write the X-Y plot and 2 lines
-png("plot3.png")
+## Create and write the X-Y plot and combine other plots
+png("plot4.png")
+par(mfrow=c(2,2), mar =c(2,2,2,2))
+plot(dataSet$datetime,dataSet$Global_active_power,type="l",xlab="", ylab="Global Active Power (kilowatts)")
+plot(dataSet$datetime,dataSet$Voltage,type="l",xlab="", ylab="Voltage")
 plot(dataSet$datetime,dataSet$Sub_metering_1,type="l",xlab=" ",ylab="Energy Sub Metering",ylim=c(0,40))
 lines(dataSet$datetime,y=dataSet$Sub_metering_2,ylim=c(0,40),col="red")
 lines(dataSet$datetime,y=dataSet$Sub_metering_3,ylim=c(0,40),col="blue")
-legend("topright", col = c("black", "red", "blue"), lty = c("solid", "solid", "solid"), legend = names(dataSet[,7:9]))
+legend("topright", cex=0.95, bty="n", col = c("black", "red", "blue"), lty = c("solid", "solid", "solid"), legend = names(dataSet[,7:9]))
+plot(dataSet$datetime,dataSet$Global_reactive_power,type="l",xlab="", ylab="Global_reactive_power")
+
 dev.off()
